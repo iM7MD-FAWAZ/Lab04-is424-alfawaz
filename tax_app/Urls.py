7d202e310898fe67e3a26c50726719c68b81c9e1
iM7MD-FAWAZ <156958892+iM7MD-FAWAZ@users.noreply.github.com>
2026-05-15 22:32:07 +0300

    Update Urls.py

--- tax_app/Urls.py
+++ tax_app/Urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('<int:price>', views.calculate_tax, name='calculate_tax'),
+    path('taxrate', views.tax_rate_view, name='tax_rate'),
+]
