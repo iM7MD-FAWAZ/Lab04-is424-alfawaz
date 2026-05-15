c226c4e5242bb62fb10722ba15b061b2eefad4c6
iM7MD-FAWAZ <156958892+iM7MD-FAWAZ@users.noreply.github.com>
2026-05-15 22:37:34 +0300

    Update urls.py

--- tax_project/urls.py
+++ tax_project/urls.py
@@ -1,1 +1,11 @@
+"""
+URL configuration for tax_project project.
 
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('tax_app.urls')),
+]
